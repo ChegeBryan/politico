0a10648b487b6060560e2ba1c6641a58da3e73af
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-04 09:06:14 +0300

    [Chore #163683369] Register run command with for application entry point

--- manage.py
+++ manage.py
@@ -10,3 +10,10 @@ app = create_app(os.getenv("APP_SETTING") or "production")
 
 # instantiate manager with the flask instance
 manager = Manager(app)
+
+@manager.command
+def run():
+    app.run()
+
+if __name__ == '__main__':
+    manager.run()
