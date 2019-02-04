c878d307dc45ae4f463afed0799ce30b414f71b5
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-04 08:49:17 +0300

    [Chore #163683369] Add application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,24 @@ app / __init.py
 Creation of application factory
 """
 
+from flask import Flask
+
+from instance.config import config_environment
+
 
 def create_app(config_name):
-    pass
+    """
+    App creation function
+
+    Args:
+        config_name string: environment name
+
+    return:
+         flask object
+    """
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_object(config_environment[config_name])
+
+    return app
+
 
