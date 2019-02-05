48383714ca8a9298a6824d96e02839aab8bb29e8
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 03:45:03 +0300

    [Feature #163683922] Add party object creation test

--- app/tests/test_party_model.py
+++ app/tests/test_party_model.py
@@ -19,4 +19,11 @@ class TestPartyModel(unittest.TestCase):
             'party name',
             'party location'
         )
-    
+
+    def test_party_object_creation(self):
+        """
+        Test party object is initialized properly
+        """
+        self.assertTrue(self.new_party.party_id)
+        self.assertEqual(self.new_party.party_name, 'party name')
+        self.assertEqual(self.new_party.hq_address, 'party location')
