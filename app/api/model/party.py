2fce7d9b310c56867a53d5cb179d0aeca57b3e15
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 03:48:00 +0300

    [Feature #163683922] Add party class init method

--- app/api/model/party.py
+++ app/api/model/party.py
@@ -6,4 +6,8 @@ class Party:
     """
     Party model class
     """
-    pass
+    def __init__(self, party_name, hq_address):
+        self.party_id = str(uuid.uuid4())
+        self.party_name = party_name
+        self.hq_address = hq_address
+
