9652ee3d1184f59caddbf7830429695347443b76
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 18:23:40 +0300

    [Feature #163683922] Add HQ's address string length validator

--- app/api/util/dto.py
+++ app/api/util/dto.py
@@ -8,8 +8,10 @@ class PartySchema(ma.Schema):
     Party schema mapped onto Party() class attributes
     """
     party_id = fields.UUID()
-    party_name = fields.String(required=True, validate=[validate.Length(min=1, error="Party name cannot be empty")])
-    hq_address = fields.String(required=True)
+    party_name = fields.String(required=True, validate=[
+                               validate.Length(min=1, error="Party name cannot be empty")])
+    hq_address = fields.String(required=True, validate=[
+                               validate.Length(min=1, error="Please provide party Headquarters address.")])
 
 
 party_schema = PartySchema()
