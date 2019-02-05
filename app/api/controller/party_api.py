9032ee5c6a66d5b73f842d34efa7f404ba3caf4f
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 14:33:22 +0300

    [Feature #163683922] Instantiate a blueprint instance on Party routes

--- app/api/controller/party_api.py
+++ app/api/controller/party_api.py
@@ -1,7 +1,9 @@
-from flask import request
+from flask import request, Blueprint
 from flask.views import MethodView
 
 from app.api.service.party import save_new_party
+
+parties = Blueprint('parties', __name__, url_prefix='/api/v1')
 
 
 class PartiesAPI(MethodView):
