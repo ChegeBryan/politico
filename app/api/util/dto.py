4d58dfadbf8504a9d4f09f6dcbdaef6798e83f62
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 10:34:43 +0300

    [Feature #163683922] Add Party schema

--- app/api/util/dto.py
+++ app/api/util/dto.py
@@ -3,6 +3,15 @@ from flask_marshmallow import Marshmallow
 
 ma = Marshmallow()
 
+class PartySchema(ma.Schema):
+    """
+    Party schema mapped onto Party() class attributes
+    """
+    party_id = fields.UUID(dump_only=True)
+    party_name = fields.String(required=True)
+    hq_address = fields.String(required=True)
 
+
+party_schema = PartySchema()
 
 
