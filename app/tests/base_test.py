76d93941668cc4dd6ec79344f38ee7832b0c78f8
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 04:48:23 +0300

    [Feature #163683922] Add party dummy test data

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -3,6 +3,7 @@
 import unittest
 
 from app import create_app
+from app.api.model.party import Party
 
 
 class BaseTestData(unittest.TestCase):
@@ -12,3 +13,18 @@ class BaseTestData(unittest.TestCase):
     def setUp(self):
         """ Initialize app and the tests data """
         self.app = create_app('testing')
+
+        # dummy data for the tests
+        self.party = Party(party_name='example name', hq_address='example location')
+        self.party_holder = self.party.party_jsonified()
+        self.null_party_name = Party(party_name='', hq_address='example location')
+        self.null_party_name_holder = self.null_party_name.party_jsonified()
+        self.null_party_hq = Party(party_name='example name', hq_address='')
+        self.null_party_hq_holder = self.null_party_hq.party_jsonified()
+        self.null_party_entries = Party(party_name='', hq_address='')
+        self.null_party_entries_holder = self.null_party_entries.party_jsonified()
+        self.int_party_name = Party(party_name='12233', hq_address='example location')
+        self.int_party_name_holder = self.int_party_name.party_jsonified()
+        self.party_name_length = Party(party_name='rt', hq_address='example location')
+        
+
