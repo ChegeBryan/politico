fb47aad2ac682758bff392eaf60e3fa05c20dce6
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-05 11:08:00 +0300

    [Feature #163683922] Deserialize Party details inputs

--- app/api/service/party.py
+++ app/api/service/party.py
@@ -2,6 +2,20 @@
 
 from app.api.db.mock_db import MockDB
 from app.api.model.party import Party
+from app.api.util.dto import party_schema
+
+def save_new_party(json_data):
+    # Deserialize the data input against the party schema
+    data = party_schema.load(json_data)
+
+    party_name = data['party_name']
+    hq_address = data['hq_address']
+
+    new_party = Party(
+        party_name=party_name,
+        hq_address=hq_address
+    )
+    save_changes(new_party)
 
 
 def save_changes(data):
