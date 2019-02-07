10bc2294beb7ac7f2ec54e83e263b7331b97e08d
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-07 21:41:09 +0300

    [Feature #163684360] Add party delete endpoint

--- app/api/controller/party_api.py
+++ app/api/controller/party_api.py
@@ -1,7 +1,7 @@
 from flask import request, Blueprint
 from flask.views import MethodView
 
-from app.api.service.party import save_new_party, get_party, get_parties, edit_party
+from app.api.service.party import save_new_party, get_party, get_parties, edit_party, delete_party
 
 parties = Blueprint('parties', __name__)
 
@@ -28,13 +28,15 @@ class PartiesAPI(MethodView):
         json_input = request.get_json()
         return edit_party(_id=_id, json_data=json_input)
 
-
+    def delete(self, _id):
+        # delete a political party
+        return delete_party(_id=_id)
 
 # register the class as view
 parties_view = PartiesAPI.as_view('parties')
 
 # api endpoints rules
 parties.add_url_rule('/parties', view_func=parties_view, methods=["POST"])
-parties.add_url_rule('/parties/<uuid:_id>', view_func=parties_view, methods=["GET"])
+parties.add_url_rule('/parties/<uuid:_id>', view_func=parties_view, methods=["GET", "DELETE"])
 parties.add_url_rule('/parties', defaults={'_id': None}, view_func=parties_view, methods=["GET"])
 parties.add_url_rule('/parties/<uuid:_id>/edit', view_func=parties_view, methods=["PUT"])
