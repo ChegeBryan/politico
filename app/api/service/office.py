b6329527c3f6962c6fdc78d69cce9cebcd867fa7
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-07 12:15:23 +0300

    [Feature #163684227] Add method to save office

--- app/api/service/office.py
+++ app/api/service/office.py
@@ -4,8 +4,22 @@ in the mock db
 """
 
 from app.api.db.mock_db import MockDB
+from app.api.model.office import Office
+from app.api.util.dto import office_schema
 
 
+def save_new_office(json_data):
+    """ Method to save new office to list """
+    # Deserialize the data input against the office schema
+    data = office_schema.load(json_data)
+    officeName = data["officeName"]
+    officeType = data["officeType"]
+    isOccupied = data["isOccupied"]
+
+    # create office object
+    new_office = Office(officeName=officeName, officeType=officeType, isOccupied=isOccupied)
+
+    save_changes(new_office)
 
 def save_changes(data):
     """ Write to the mock db """
