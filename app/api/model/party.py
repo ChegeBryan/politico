de8472191d632fdc72971a5f23887a872f0b2b9d
Chege Bryan <cbbryan8@gmaiil.com>
2019-02-07 21:43:32 +0300

    [Feature #163684360] Add cls argument to delete method

--- app/api/model/party.py
+++ app/api/model/party.py
@@ -47,7 +47,7 @@ class Party:
         return party_to_edit
 
     @classmethod
-    def delete_party(_id):
+    def delete_party(cls, _id):
         """ Method to delete party from parties list """
         for party in MockDB.PARTIES:
             if party.party_id == _id:
