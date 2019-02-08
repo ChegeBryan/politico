4ce11a455436c03699ea14c8e325a2d65187b7b3
ChegeBryan <cbbryan8@gmail.com>
2019-02-08 10:51:29 +0300

    [Bug #163812299] Match app file indentation

--- app/__init__.py
+++ app/__init__.py
@@ -11,33 +11,29 @@ from app.api.controller.office_api import offices as office_bp
 
 
 def create_app(config_name):
-    """
-    App creation function
+     """
 
-    Args:
+     App creation function
+     Args:
         config_name string: environment name
+     return:
+        flask object
+     """
+     app = Flask(__name__, instance_relative_config = True)
+     app.config.from_object(config_environment[config_name])
 
-    return:
-         flask object
-    """
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(config_environment[config_name])
+     # Register app error handlers
+     @app.errorhandler(405)
+     def method_not_allowed(e):
+          """
+          Json response for Method Not Allowed.
+          """
+          return jsonify({
+               "status": 405,
+               "error": "Method Not Allowed on Url."
+          }), 405
+     # register blueprints to app
+     app.register_blueprint(parties_bp, url_prefix='/api/v1')
+     app.register_blueprint(office_bp, url_prefix='/api/v1')
 
-    # Register app error handlers
-    @app.errorhandler(405)
-    def method_not_allowed(e):
-        """
-        Json response for Method Not Allowed.
-        """
-        return jsonify({
-            "status": 405,
-            "error": "Method Not Allowed on Url."
-        }), 405
-
-
-
-    # register blueprints to app
-    app.register_blueprint(parties_bp, url_prefix='/api/v1')
-    app.register_blueprint(office_bp, url_prefix='/api/v1')
-
-    return app
+     return app
