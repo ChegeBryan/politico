42cfe2df1e721ebc609814a19a33278fb658ce1d
ChegeBryan <cbbryan8@gmail.com>
2019-02-08 10:45:51 +0300

    [Starts #163812299] Add error handler for method not allowed

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ app / __init.py
 Creation of application factory
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 
 from instance.config import config_environment
 from app.api.controller.party_api import parties as parties_bp
@@ -20,13 +20,24 @@ def create_app(config_name):
     return:
          flask object
     """
-    app = Flask(__name__, instance_relative_config = True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_environment[config_name])
 
+    # Register app error handlers
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        """
+        Json response for Method Not Allowed.
+        """
+        return jsonify({
+            "status": 405,
+            "error": "Method Not Allowed on Url."
+        }), 405
+
+
+
     # register blueprints to app
-    app.register_blueprint(parties_bp, url_prefix = '/api/v1')
+    app.register_blueprint(parties_bp, url_prefix='/api/v1')
     app.register_blueprint(office_bp, url_prefix='/api/v1')
 
     return app
-
-
