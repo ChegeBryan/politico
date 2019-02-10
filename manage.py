e1402aa08ca8dd1c3d730d3d8d68f8c0a778c01b
ChegeBryan <cbbryan8@gmail.com>
2019-02-10 12:07:28 +0300

    [Chore #163846710] Remove unused app dependencies

--- manage.py
+++ manage.py
@@ -2,18 +2,9 @@
 
 import os
 
-from flask_script import Manager
-
 from app import create_app
 
 app = create_app(os.getenv("APP_SETTING") or "production")
 
-# instantiate manager with the flask instance
-manager = Manager(app)
-
-@manager.command
-def run():
-    app.run()
-
 if __name__ == '__main__':
-    manager.run()
+    app.run()
