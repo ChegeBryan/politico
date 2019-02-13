4566c0ecd99da78c3be408a52457b24c7dbced68
ChegeBryan <cbbryan8@gmail.com>
2019-02-13 23:54:56 +0300

    [Chore #163916330] Move relations to db_models

---
+++ app/api/db/db_models.py
@@ -0,0 +1,22 @@
+# database relations.
+
+
+def create_tables():
+    """Create relations in the database"""
+    users = """
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        firstname VARCHAR(20) NOT NULL,
+        lastname VARCHAR(20) NOT NULL,
+        othername VARCHAR(20),
+        email VARCHAR(30) NOT NULL,
+        phonenumber VARCHAR(24) NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        passportUrl VARCHAR(256) NOT NULL,
+        isAdmin BOOLEAN DEFAULT FALSE,
+        isPolitician BOOLEAN DEFAULT FALSE
+    );
+    """
+    return [users]
+
+
