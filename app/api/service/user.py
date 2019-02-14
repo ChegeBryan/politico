c653b90820fd7cb33c6786678d8d7924a89edc6b
ChegeBryan <cbbryan8@gmail.com>
2019-02-14 08:47:35 +0300

    [Feature #163964899] Add skeleton user model and methods

---
+++ app/api/service/user.py
@@ -0,0 +1,17 @@
+""" Methods that involve data manipulation in the database """
+
+def save_new_user(json_data):
+    """ Method to save a new user to the database """
+    pass
+
+def get_user():
+    """ Method to get the one single record from users relations. """
+    pass
+
+def get_all_users():
+    """ Method to get all users in the users relations """
+    pass
+
+def save_changes(data):
+    """ Write to the database """
+    pass
