e8611a62f86b290a8eacf4aabbf033a6c25b57ad
ChegeBryan <cbbryan8@gmail.com>
2019-02-14 18:25:57 +0300

    [Feature #163964899] Add method to get json body for user registration

--- app/api/service/user.py
+++ app/api/service/user.py
@@ -1,8 +1,39 @@
 """ Methods that involve data manipulation in the database """
+from flask import jsonify
+
+from app.api.model.user import User
+from app.api.util.dto import user_schema
+from app.api.model.user import User
+
 
 def save_new_user(json_data):
     """ Method to save a new user to the database """
-    pass
+
+    # 1. Deserialize the data input against the user schema
+
+    data = user_schema.load(json_data)
+
+    firstname = data["firstname"]
+    lastname = data["lastname"]
+    othername = data["othername"]
+    email = data["email"]
+    phonenumber = data["phonenumber"]
+    password = data["password"]
+    passportUrl = data["passportUrl"]
+    isAdmin = data["isAdmin"]
+    isPolitician = data["isPolitician"]
+
+    new_user = User(firstname=firstname, lastname=lastname, othername=othername,email=email, phonenumber=phonenumber, password=password, passportUrl=passportUrl, isAdmin=isAdmin, isPolitician=isPolitician)
+
+    save_changes(new_user)
+    # 1. Serialize the input for response
+    # 2. Return serialized and proper format json to api endpoint
+    response = user_schema.dump(new_user)
+    response_object = jsonify({
+        "status": 201,
+        "data": [response]
+    })
+    return response_object, 201
 
 def get_user():
     """ Method to get the one single record from users relations. """
