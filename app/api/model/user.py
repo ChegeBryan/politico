bc3e844e8c295471f96c231fcc8a4b02a5bd963c
ChegeBryan <cbbryan8@gmail.com>
2019-02-14 09:50:49 +0300

    [Feature #163964899] Add user object init method

--- app/api/model/user.py
+++ app/api/model/user.py
@@ -1,6 +1,17 @@
-
+""" User class methods and model"""
 
 class User:
+    """  User model class """
 
-    def __init__(self, **kwargs):
-        pass
+    # 1. initialize object with isAdmin and isPolitician set to default False
+    # 2. Read the class variable form named arguments **kwargs
+    def __init__(self, isAdmin=False, isPolitician=False, **kwargs):
+        self.firstname = kwargs.get("firstname")
+        self.lastname = kwargs.get("lastname")
+        self.othername = kwargs.get("othername")
+        self.email = kwargs.get("email")
+        self.phonenumber = kwargs.get("phonenumber")
+        self.password = kwargs.get("password")
+        self.passportUrl = kwargs.get("passportUrl")
+        self.isAdmin = isAdmin
+        self.isPolitician = isPolitician
