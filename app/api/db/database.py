8e7b2604ac785ba1d825b59b9cbfee993d9deb88
ChegeBryan <cbbryan8@gmail.com>
2019-02-16 20:44:24 +0300

    [Feature #163964899] Get database dsn from current app context

--- app/api/db/database.py
+++ app/api/db/database.py
@@ -5,25 +5,32 @@ import os
 
 import psycopg2
 
+from flask import current_app
+
 from psycopg2.extras import RealDictCursor as dict_cursor
 from app.api.db.db_models import create_tables, drop_tables
 
-from instance.config import config_environment
-
-use_db = os.getenv("APP_SETTING")
-dsn = config_environment[use_db].DATABASE_DSN
 
 class AppDatabase:
+    """Database connection class
+    Methods that:
+       1. connect to database
+       2. write, read and update database entries reside here.
+    """
 
-    def __init__(self, dsn):
+    def __init__(self):
         """class instance to create a database connection.
 
         Args:
-            dsn (libpd connection string): the connection parameters for the database.
+            dsn (libpd connection string): the connection parameters for the
+            database.
+            When the class is initialized a connection to the database is
+            established depending on the environment the app is running at.
         """
         try:
+            dsn = current_app.config["DATABASE_DSN"]
             self.conn = psycopg2.connect(dsn)
-            self.cur = self.conn.cursor()
+            self.cur = self.conn.cursor(cursor_factory=dict_cursor)
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
@@ -59,5 +66,4 @@ class AppDatabase:
         # execute the queries to drop the tables
         for query in queries:
             self.cur.execute(query)
-
         self.conn.commit()
