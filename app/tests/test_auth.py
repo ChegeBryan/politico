262814dc8847e7926f8482f8b08cfb7b62365bc2
ChegeBryan <cbbryan8@gmail.com>
2019-02-19 12:43:44 +0300

    [Feature #163965315] Add registered user login test

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -8,7 +8,24 @@ class UserAuthTesCases(BaseTestData):
     """
     Test cases class for testing user login and authentication
     """
-    pass
+
+    def test_registered_user_login(self):
+        """
+        Test if a registered user can login to the system.
+        STATUS CODE: 200 Ok
+        """
+        # register a user
+        register_user = self.user_data
+        self.assertEqual(register_user.status_code, 201)
+
+        # login registered user
+        user_login = self.login_data
+        json_body = user_login.get_json()
+        self.assertEqual(json_body["status"], 200)
+        self.assertEqual(json_body["message"], "Successfully logged in.")
+        self.assertEqual(user_login.status_code, 200)
+
+
 
 
 
