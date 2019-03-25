2ec3d1d95bac7420733cf533a5cc88305e05510f
ChegeBryan <cbbryan8@gmail.com>
2019-03-26 01:35:44 +0300

    [Feature #164881487] Add SQL query to add office to database

--- app/api/model/office.py
+++ app/api/model/office.py
@@ -14,6 +14,21 @@ class Office:
         self.office_type = office_type
         self.is_occupied = False
 
+    def add_office(self):
+        """ add an office object to the parties table
+
+        Returns:
+            tuple : insert sql query, values(the object initialization
+                    attributes)
+        """
+        query = """INSERT INTO
+          offices(office_name, office_type, is_occupied)
+          VALUES (%s,%s,%s);
+          """
+        values = (self.office_name, self.office_type, self.is_occupied)
+
+        return query, values
+
     @staticmethod
     def get_office_by_id(identifier):
         """Method to get office in the OFFICES list by its ID"""
