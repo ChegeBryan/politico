25eab26463904baa9008b6343cb01c587dfc611c
ChegeBryan <cbbryan8@gmail.com>
2019-03-26 01:17:48 +0300

    [Feature #164881487] Rewrite the office dummy tests to dictionaries

--- app/api/db/office_test_data.py
+++ app/api/db/office_test_data.py
@@ -1,13 +1,20 @@
 # office dummy test data
-from app.api.model.office import Office
-
 
 # data to use for office test
-office = Office('senator', 'congress', False)
-office_holder = office.office_jsonified()
+office_holder = {
+    "office_name": 'senator',
+    "office_type": "congress",
+    "is_occupied": False
+}
 
-invalid_office_name = Office('sene', "congress", False)
-invalid_office_name_holder = invalid_office_name.office_jsonified()
+invalid_office_name_holder = {
+    "office_name": 'sene',
+    "office_type": "congress",
+    "is_occupied": False
+}
 
-invalid_office_type = Office('senator', "cong", False)
-invalid_office_type_holder = invalid_office_type.office_jsonified()
+invalid_office_type_holder = {
+    "office_name": 'senator',
+    "office_type": "cong",
+    "is_occupied": False
+}
