46b56bb73ce5d199488bc938b95cf03f356b4ff9
ChegeBryan <cbbryan8@gmail.com>
2019-03-28 12:46:33 +0300

    [Feature #163965831] Add candidates blueprint endpoint rules

--- app/api/controller/candidate_api.py
+++ app/api/controller/candidate_api.py
@@ -1,9 +1,11 @@
 """  views """
-from flask import request
+from flask import request, Blueprint
 from flask.views import MethodView
 
 from app.api.service.candidate import save_new_candidate
 from app.api.util.decorator import admin_token_required
+
+candidates = Blueprint('candidates', __name__)
 
 
 class CandidateAPI(MethodView):
@@ -18,3 +20,7 @@ class CandidateAPI(MethodView):
 
 # register the class as view
 candidates_view = CandidateAPI.as_view('candidates')
+
+# api endpoints rules
+candidates.add_url_rule('/office/<int:_id>/register',
+                        view_func=candidates_view, methods=["POST"])
