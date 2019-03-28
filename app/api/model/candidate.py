cf1508f1c1bc4d32218b91649baf3bfb038ea66b
ChegeBryan <cbbryan8@gmail.com>
2019-03-28 11:03:48 +0300

    [Feature #163965831] SQL query to add candidate to database

--- app/api/model/candidate.py
+++ app/api/model/candidate.py
@@ -12,3 +12,18 @@ class Candidate:
         self.office = office
         self.party = party
         self.created_on = datetime.datetime.now()
+
+    def add_candidate(self, office_id):
+        """SQL query to add a candidate to the candidates table
+
+        Args:
+            office_id (integer): office to register a candidate to
+        """
+        query = """
+        INSERT INTO
+          candidates(office_id, candidate_id, party_id, created_on)
+          VALUES(%s, %s, %s, %s);
+        """
+        values = (office_id, self.candidate, self.party, self.created_on)
+
+        return query, values
