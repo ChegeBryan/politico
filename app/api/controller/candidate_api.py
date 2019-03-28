86a688531f615ecbf56f9948488b9c8a2599be0b
ChegeBryan <cbbryan8@gmail.com>
2019-03-28 11:55:36 +0300

    [Feature #163965831] Add candidate registraion view method

---
+++ app/api/controller/candidate_api.py
@@ -0,0 +1,16 @@
+"""  views """
+from flask import request
+from flask.views import MethodView
+
+from app.api.service.candidate import save_new_candidate
+from app.api.util.decorator import admin_token_required
+
+
+class CandidateAPI(MethodView):
+    """ Candidate method views """
+
+    @admin_token_required
+    def post(self, _id):
+        # register candidate for office
+        json_input = request.get_json()
+        return save_new_candidate(office_id=_id, json_data=json_input)
