c65d08b434fe022a74afe503df8bda66f900d77d
ChegeBryan <cbbryan8@gmail.com>
2019-03-30 10:26:43 +0300

    [Feature #163966060] Add Vote model init method

--- app/api/model/vote.py
+++ app/api/model/vote.py
@@ -1,8 +1,13 @@
 """ Vote database model and methods """
 
+import datetime as dt
+
+
 class Vote:
     """ Vote class for and methods for database manipulation """
 
-    def __init__(self):
-        pass
+    def __init__(self, office, candidate):
+        self.office = office
+        self.candidate = candidate
+        self.created_on = dt.datetime.now()
 
