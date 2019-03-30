21e52b28bdff25dd65d87ae3c56b6f5da3f01ae6
ChegeBryan <cbbryan8@gmail.com>
2019-03-30 19:29:31 +0300

    [Feature #163966060] Add authentication error test on votes endpoint

--- app/tests/test_vote_api.py
+++ app/tests/test_vote_api.py
@@ -1,6 +1,7 @@
 """ Tests for the vote endpoints """
 
 from .base_test import BaseTestData
+from app.api.db.vote_test_data import vote
 
 
 class VoteAPITestCases(BaseTestData):
@@ -18,3 +19,15 @@ class VoteAPITestCases(BaseTestData):
         self.assertEqual(json_body["data"][0]["candidate"], 2)
         self.assertTrue(json_body["data"][0]["created_on"])
         self.assertEqual(response.status_code, 201)
+
+    def test_auth_token_error(self):
+        """Test api returns correct error code when the provided
+        and error is encountered during token decoding not provide a valid token.
+        """
+        # register a vote
+        response = self.client.post(
+            '/api/v2/votes', json=vote, headers={
+                "Authorization": "Bearer {}".format("malformed_token")
+            }
+        )
+        self.assertEqual(response.status_code, 401)
