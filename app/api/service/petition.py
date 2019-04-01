3592245b31c1372bbe75274f96daa06c9e67e26a
ChegeBryan <cbbryan8@gmail.com>
2019-04-01 17:37:25 +0300

    [Feature #165011227] Commit petition to database

--- app/api/service/petition.py
+++ app/api/service/petition.py
@@ -9,6 +9,7 @@ from marshmallow import ValidationError
 from app.api.model.petition import Petition
 from app.api.util.dto import petition_load_schema
 from app.api.service.auth_helper import get_logged_in_user
+from app.api.db.database import AppDatabase as db
 
 
 def save_new_petition(json_data):
@@ -41,3 +42,17 @@ def save_new_petition(json_data):
         body=body,
         evidence=evidence
     )
+
+    save_and_return_id = save_changes(new_petition)
+
+
+def save_changes(data):
+    """commit the petition details to the database
+
+    Args:
+        data (object): petition details
+    """
+    query, values = Petition.add_petition(data)
+    petition_id = db().commit_changes_returning_id(query, values)
+
+    return petition_id
