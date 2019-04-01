16d8d7d8fd0613e787edb3a5f5fc247386906f00
ChegeBryan <cbbryan8@gmail.com>
2019-04-01 14:15:40 +0300

    [Feature #165011227] Add petition model object creation test

---
+++ app/api/model/petition.py
@@ -0,0 +1,13 @@
+""" Petition database model and methods """
+
+import datetime as dt
+
+
+class Petition:
+    """Petition class methods that involve reading
+    and writing to the database
+    """
+
+    def __init__(self):
+        pass
+
