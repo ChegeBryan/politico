d73cf57cabc60e8d797d60be722428817902ddb5
ChegeBryan <cbbryan8@gmail.com>
2019-04-01 03:06:38 +0300

    [Feature #163966386] Query database for office results set

---
+++ app/api/service/result.py
@@ -0,0 +1,17 @@
+""" data manipulation  database and view """
+
+from app.api.model.result import get_office_result
+from app.api.db.database import AppDatabase as db
+
+
+def office_result(office):
+    """get the result set from the database
+
+    Args:
+        office (integer): the office to get results for
+    """
+    # sql query to get a user results
+    results_query = get_office_result(office)
+    office_result = db().get_all_rows(results_query)
+
+
