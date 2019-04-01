a56102ff066bc168e159c541bed7be1ec0678d36
ChegeBryan <cbbryan8@gmail.com>
2019-04-01 04:41:27 +0300

    [Finishes #163966386] Verify user is authorized to view results endpoint

--- app/api/service/result.py
+++ app/api/service/result.py
@@ -1,7 +1,9 @@
 """ data manipulation  database and view """
 
-from flask import jsonify
+from flask import jsonify, request
 
+
+from app.api.service.auth_helper import get_logged_in_user
 from app.api.model.result import get_office_result
 from app.api.util.dto import results_schema
 from app.api.db.database import AppDatabase as db
@@ -13,23 +15,29 @@ def office_result(office):
     Args:
         office (integer): the office to get results for
     """
-    # sql query to get a user results
-    results_query = get_office_result(office)
-    office_result = db().get_all_rows_of_value(*results_query)
+    # verify the token provided by user is valid
+    response, status = get_logged_in_user(request)
 
-    if office_result:
-        # serialize office results
-        serialize_results = results_schema.dump(office_result)
+    if status == 200:
+        # sql query to get a user results
+        results_query = get_office_result(office)
+        office_result = db().get_all_rows_of_value(*results_query)
 
-        # json results response
+        if office_result:
+            # serialize office results
+            serialize_results = results_schema.dump(office_result)
+
+            # json results response
+            response_object = jsonify({
+                "status": 200,
+                "data": serialize_results
+            })
+            return response_object, 200
+        # when no result is returned
         response_object = jsonify({
-            "status": 200,
-            "data": serialize_results
+            "status": 404,
+            "message": "Results requested not found."
         })
-        return response_object, 200
-    # when no result is returned
-    response_object = jsonify({
-        "status": 404,
-        "message": "Results requested not found."
-    })
-    return response_object, 404
+        return response_object, 404
+    # if user could not be verified
+    return response, status
