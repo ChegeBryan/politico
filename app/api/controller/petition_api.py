8f1c9d2a8e8607d9966180baccb30c1ed4dee395
ChegeBryan <cbbryan8@gmail.com>
2019-04-01 18:01:27 +0300

    [Feature #165011227] Add petitions endpoint post method rule

--- app/api/controller/petition_api.py
+++ app/api/controller/petition_api.py
@@ -1,10 +1,13 @@
 """petition view functions """
 
-from flask import request
+from flask import request, Blueprint
 from flask.views import MethodView
 
 from app.api.service.petition import save_new_petition
 from app.api.util.decorator import token_required
+
+
+petitions = Blueprint('petitions', __name__)
 
 
 class PetitionsAPI(MethodView):
@@ -21,3 +24,7 @@ class PetitionsAPI(MethodView):
 
 # register the class as view
 petitions_view = PetitionsAPI.as_view('petitions')
+
+# api endpoints rules
+petitions.add_url_rule(
+    '/petitions', view_func=petitions_view, methods=["POST"])
