9d2b4004e6bfbcb05d4b8fc8e790682d48923d6f
ChegeBryan <cbbryan8@gmail.com>
2019-05-13 09:49:57 +0300

    [Feature #165906293] Move shared variables to helpers module

---
+++ UI/js/api/helpers.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview contains variables and functions that ares shared
+ * @exports base api URL, formDataToJson function
+ *
+ */
+
+// ? create API base URL
+export const baseURL = new URL('https://politico-cb.herokuapp.com');
+
+/**
+ * @function {formDataToJson} creates a json string from form data
+ * @param {obj} Form input values
+ * @returns {json} json string with form data values and keys
+ *
+ */
+export function formDataToJson(formData) {
+  const JSONFormDataObj = {};
+  // ? populate the json object with key-value pair
+  formData.forEach((value, key) => {
+    JSONFormDataObj[key] = value;
+  });
+  const formDataJSON = JSON.stringify(JSONFormDataObj);
+  return formDataJSON;
+}
