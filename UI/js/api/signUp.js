af16e94fc29ce95dce56ec04444cd4a380c03cba
ChegeBryan <cbbryan8@gmail.com>
2019-05-20 14:43:01 +0300

    [Feature #165906352] Create user signup Auth class instance

---
+++ UI/js/api/signUp.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview contains logic specific to user login
+ * @imports Auth class, signUpApiUrl
+ *
+ */
+
+import Auth from './auth.js'
+import {
+  signUpApiUrl
+} from './helpers.js'
+
+// user login form submit button
+const signUpBtn = document.querySelector('#signup-btn');
+
+/**
+ * @event click create a user registration instance from Auth class
+ *  when a submits the registration form
+ */
+signUpBtn.addEventListener('click', () => {
+  // create AUth class instance for user registration
+  const signUpForm = document.querySelector('#signup-form');
+  const userSignUp = new Auth(signUpForm, signUpApiUrl);
+  userSignUp.validateForm()
+});
