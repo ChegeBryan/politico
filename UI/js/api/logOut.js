2bf9eb05112c98aaf54ebdd573b50c4366369c6d
ChegeBryan <cbbryan8@gmail.com>
2019-05-21 19:15:31 +0300

    [Feature #165906417] Pause function till fetch resolves before redirect

--- UI/js/api/logOut.js
+++ UI/js/api/logOut.js
@@ -10,19 +10,23 @@ import {
 
 // get the logout button
 const logOutBtn = document.querySelector('#logout-btn');
-logOutBtn.addEventListener('click', () => {
+logOutBtn.addEventListener('click', async () => {
   // get the token from the local storage
   const currentUserToken = localStorage.getItem('token');
   if (currentUserToken) {
-    fetch(signOutApiUrl, {
+    /**
+     * @description call fetch but wait for the promise to resolve
+     *  before the low code is called i.e before a user is redirected
+     */
+    await fetch(signOutApiUrl, {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${currentUserToken}`
         },
       })
       .then(validateResponse)
-      .then(localStorage.removeItem('token')) // clear token from client localstorage
-      .then(window.location.replace('signin.html'))
       .catch(alertError);
   }
+  localStorage.removeItem('token');
+  window.location.replace('signin.html');
 });
