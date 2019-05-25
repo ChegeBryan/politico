46dda58c9d1ae9621a387f5fc504096bd3968805
ChegeBryan <cbbryan8@gmail.com>
2019-05-25 20:12:35 +0300

    [Feature #166178952] Submit party registration form data

---
+++ UI/js/api/controller/addParty.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Party registration controller
+ */
+
+import Party from '../model/party.js';
+import {
+  partiesApiUrl
+} from '../helpers.js';
+
+
+const addPartyBtn = document.querySelector('#add-party-btn');
+addPartyBtn.addEventListener('click', () => {
+  // party registration form
+  const currentUserToken = localStorage.getItem('token');
+  const form = document.querySelector('#add-party-form');
+  // check if the form data is valid before submiting
+  if (form.checkValidity()) {
+    /**
+     * Party instance, to aceess the add party method for party
+     * party registration
+     * @instance
+     */
+    const registerParty = new Party(currentUserToken, partiesApiUrl, form);
+    registerParty.addParty();
+    return;
+  }
+  alert('Fix highlighted form error and submit again');
+});
