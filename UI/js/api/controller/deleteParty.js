0ae8b8ca699575a64ed51a36ebc6057633c6917b
ChegeBryan <cbbryan8@gmail.com>
2019-06-03 15:48:51 +0300

    [Feature #166179115] Populate delete party table on page load

---
+++ UI/js/api/controller/deleteParty.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview handles DOM events on delete party page
+ */
+
+import {
+  PartyAdminAccess
+} from "../model/party.js";
+import {
+  partiesApiUrl
+} from "../helpers.js";
+
+/**
+ * @description fetches parties details from the server
+ */
+const fetchParties = () => {
+  const currentUser = localStorage.getItem('token');
+  // call the getParties from the Party class
+  PartyAdminAccess.deletePartiesList(partiesApiUrl, currentUser);
+}
+
+/**
+ * call the fetch parties method whenever the edit page is loaded
+ * or refreshed
+ * @event load
+ */
+window.addEventListener('load', fetchParties);
