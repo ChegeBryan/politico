bb83a179a676efeff42cba01c669779f99bd3dc1
ChegeBryan <cbbryan8@gmail.com>
2019-06-05 12:14:04 +0300

    [Feature #166178702] Auto select office type

--- UI/js/api/controller/addOffice.js
+++ UI/js/api/controller/addOffice.js
@@ -9,6 +9,29 @@ import {
  * @fileoverview Office registration controller
  */
 
+
+/**
+ * listens for a click event the office name inputs
+ * and automatically selects the office type
+ */
+const officeNameInputs = document.querySelectorAll('input[name="office_name"]');
+officeNameInputs.forEach(officeNameInput => {
+  // automatically select the office type based on checked office name
+  officeNameInput.addEventListener('click', () => {
+    if (officeNameInput.value === 'president') {
+      const officeTypeInput = document.querySelector('input[value="federal"]');
+      officeTypeInput.checked = true;
+    } else if (officeNameInput.value === 'house of representatives' ||
+      officeNameInput.value === 'senator') {
+      const officeTypeInput = document.querySelector('input[value="congress"]');
+      officeTypeInput.checked = true;
+    } else if (officeNameInput.value === 'governor') {
+      const officeTypeInput = document.querySelector('input[value="state"]');
+      officeTypeInput.checked = true;
+    }
+  });
+});
+
 const addOfficeBtn = document.querySelector('#add-office-btn');
 addOfficeBtn.addEventListener('click', () => {
   const currentUserToken = localStorage.getItem('token');
