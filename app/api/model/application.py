2c21aacb03865780dbc752bed514d22b0d0b35b7
ChegeBryan <cbbryan8@gmail.com>
2019-06-10 00:39:21 +0300

    [Feature #166551893] Add application class properties

--- app/api/model/application.py
+++ app/api/model/application.py
@@ -1,9 +1,20 @@
 """ Applications database model and methods """
 
+import datetime
+
 
 class Application:
     """Application class for and methods for database manipulation
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, party, office):
+        """application class properties registered when an instance of this
+        class is created
+
+        Args:
+            party (int): id of a registered party
+            office (int): id of a registered office
+        """
+        self.party = party
+        self.office = office
+        self.requested_on = datetime.datetime.now()
