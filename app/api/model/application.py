f6f036e0dd3a9080fcbbd0f85c54c5dc26cf5a57
ChegeBryan <cbbryan8@gmail.com>
2019-06-10 00:32:11 +0300

    [Feature #166551893] Test application class object creation

---
+++ app/api/model/application.py
@@ -0,0 +1,9 @@
+""" Applications database model and methods """
+
+
+class Application:
+    """Application class for and methods for database manipulation
+    """
+
+    def __init__(self):
+        pass
