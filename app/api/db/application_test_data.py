2e2de79959e5afa5de3cb0891123497961cc2e71
ChegeBryan <cbbryan8@gmail.com>
2019-06-13 20:56:46 +0300

    [Feature #166551893] Add office application on base tests

---
+++ app/api/db/application_test_data.py
@@ -0,0 +1,6 @@
+# dummy data for application api
+
+application = {
+    "party": "example name",  # same party name registered during test
+    "office": "senator"  # same office created during during test
+}
