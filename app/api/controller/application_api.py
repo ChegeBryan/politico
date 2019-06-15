b6f5e120652127e71724d8d57d456528ef77ee93
ChegeBryan <cbbryan8@gmail.com>
2019-06-15 21:39:08 +0300

    [Feature #166551893] Add application registration view method

---
+++ app/api/controller/application_api.py
@@ -0,0 +1,19 @@
+""" Application views """
+from flask import request
+from flask.views import MethodView
+
+
+from app.api.service.application import save_new_application
+from app.api.util.decorator import token_required
+
+
+class ApplicationApi(MethodView):
+    """
+    Application endpoint
+    """
+
+    @token_required
+    def post(self):
+        # register user office application
+        json_input = request.get_json()
+        return save_new_application(json_input)
