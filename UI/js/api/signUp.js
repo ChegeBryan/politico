c5a5742eac50914276dd89773da3511f74b699c1
ChegeBryan <cbbryan8@gmail.com>
2019-06-18 16:14:50 +0300

    [Bug #166761703] Validate form before creating user signup instance

--- UI/js/api/signUp.js
+++ UI/js/api/signUp.js
@@ -19,6 +19,17 @@ const signUpBtn = document.querySelector('#signup-btn');
 signUpBtn.addEventListener('click', () => {
   // create AUth class instance for user registration
   const signUpForm = document.querySelector('#signup-form');
-  const userSignUp = new Auth(signUpForm, signUpApiUrl);
-  userSignUp.validateForm();
+
+  // check if the form data is valid before submiting
+  if (signUpForm.checkValidity()) {
+    /**
+     * Auth instance, to access method for sending form data
+     * party registration
+     * @instance
+     */
+    const userSignUp = new Auth(signUpForm, signUpApiUrl);
+    userSignUp.sendData();
+    return;
+  }
+  alert('Fix highlighted form error and submit again');
 });
